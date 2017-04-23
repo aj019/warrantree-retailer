238b861eaf1f3e72809900e9645cdd66d92453e7
Anuj Gupta <junagupta19@gmail.com>
2017-04-23 19:23:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/android/test/warrantree_retailer/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.android.test.warrantree_retailer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    Button btLogin,btGoToSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btLogin = (Button) findViewById(R.id.btLogin);
+        btGoToSignup = (Button) findViewById(R.id.btGoToSign);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+            }
+        });
+        btGoToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+
+            }
+        });
+
+    }
+}
